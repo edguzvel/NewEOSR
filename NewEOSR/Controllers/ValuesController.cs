cdbeee1ced897e9d70a7217f7b33ae088ef1aaee
Alberto Arciva <alberto.arciva@gmail.com>
2024-04-06 23:05:24 -0500

    Making correct info show up

--- NewEOSR/Controllers/ValuesController.cs
+++ NewEOSR/Controllers/ValuesController.cs
@@ -84,6 +84,95 @@ namespace NewEOSR.Controllers
             return new JsonResult("Deleted Successfully");
         }
 
+        [HttpPost]
+        [Route("SubmitReport")]
+        public JsonResult SubmitReport([FromBody] ReportModel report)
+        {
+            string query = @"
+            INSERT INTO Reports (userId, reportDate, reportContent) 
+            VALUES (@userId, @reportDate, @reportContent)";
+        
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@userId", report.UserId);
+                    myCommand.Parameters.AddWithValue("@reportDate", report.ReportDate);
+                    myCommand.Parameters.AddWithValue("@reportContent", report.ReportContent);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Report Submitted Successfully");
+        }
+
+        [HttpGet]
+        [Route("GetReportsByUser")]
+        public JsonResult GetReportsByUser(int userId)
+        {
+            string query = @"
+                SELECT * FROM Reports 
+                WHERE userId = @userId";
+                
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@userId", userId);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); 
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+        [Route("GetUserById")]
+        public JsonResult GetUserById(int userId)
+        {
+            string query = "SELECT * FROM dbo.tblUserss WHERE id = @userId";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@userId", userId);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+
+
+public class ReportModel
+{
+    public int UserId { get; set; }
+    public DateTime ReportDate { get; set; }
+    public string ReportContent { get; set; }
+}
+
+
         
     }
 }
